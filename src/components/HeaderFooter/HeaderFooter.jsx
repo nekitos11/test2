dce3af9160234cb03b4202b849507ce69cb80f82
Жолобов Никита Евгеньевич <NZHolobov@IBS.RU>
2024-03-08 10:17:32 +0300

    fix

--- src/components/HeaderFooter/HeaderFooter.jsx
+++ src/components/HeaderFooter/HeaderFooter.jsx
@@ -2,6 +2,6 @@ import styles from "./HeaderFooter.module.scss";
 
 export const HeaderFooter = () => (
   <header className={styles.header}>
-    <img src="./../../assets/images/logo.png" alt="logo" />
+    <img src="@assets/images/logo.png" alt="logo" />
   </header>
 );
